6fe731a699059dd95649713f340b0da2abfdd48b
RichardMartins99 <77687618+RichardMartins99@users.noreply.github.com>
2023-04-16 15:34:59 -0300

    commit

--- ProblemaComOrientacaoObjetos/ProblemaComOrientacaoObjetos/ProblemaComOrientacaoObjetos/Triangulo.cs
+++ ProblemaComOrientacaoObjetos/ProblemaComOrientacaoObjetos/ProblemaComOrientacaoObjetos/Triangulo.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProblemaComOrientacaoObjetos
+﻿namespace ProblemaComOrientacaoObjetos
 {
-    internal class Triangulo
+    public class Triangulo
     {
+        public double A;
+        public double B;
+        public double C;
+
     }
 }
