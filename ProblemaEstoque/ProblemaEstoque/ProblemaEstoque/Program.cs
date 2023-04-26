50d4642446a7e393c3bb0072064bd66061c80cfa
RichardMartins99 <77687618+RichardMartins99@users.noreply.github.com>
2023-04-25 22:01:56 -0300

    exercicio Problema de estoque

---
+++ ProblemaEstoque/ProblemaEstoque/ProblemaEstoque/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace ProblemaEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            Console.WriteLine("Entre os dados do Produto:");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            produto.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do produto: " + produto);
+        }
+    }
+}
