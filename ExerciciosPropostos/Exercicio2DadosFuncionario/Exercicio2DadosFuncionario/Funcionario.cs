cf9176f81bf003db6f96e80cc50b556ba33d1b39
RichardMartins99 <77687618+RichardMartins99@users.noreply.github.com>
2023-04-28 14:24:06 -0300

    exercicio2Funcionario

---
+++ ExerciciosPropostos/Exercicio2DadosFuncionario/Exercicio2DadosFuncionario/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio2DadosFuncionario
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public double SalarioBruto { get; set; }
+        public double Imposto { get; set; }
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem) 
+        { 
+            SalarioBruto += porcentagem / 100 * SalarioBruto;
+        }
+    }
+}
---
+++ ExerciciosPropostos/Exercicio2DadosFuncionario/Exercicio2DadosFuncionario/Program.cs
@@ -0,0 +1,19 @@
+﻿using Exercicio2DadosFuncionario;
+using System.Globalization;
+
+var funcionario = new Funcionario();
+
+Console.Write("Nome: ");
+funcionario.Nome = Console.ReadLine();
+
+Console.Write("Salário bruto: ");
+funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Imposto: ");
+funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine($"Funcionário: {funcionario.Nome}, {funcionario.SalarioLiquido():F2}");
+Console.Write($"Digite a porcentagem para aumentar o salário: ");
+funcionario.AumentarSalario(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+Console.WriteLine($"Dados atualizados: {funcionario.Nome}, {funcionario.SalarioLiquido():F2}");
