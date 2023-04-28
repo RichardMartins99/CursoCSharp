c8fe848eeba09491b0b865caa76f3867cc73aded
RichardMartins99 <77687618+RichardMartins99@users.noreply.github.com>
2023-04-28 14:48:54 -0300

    exercicio3alunonotas

---
+++ ExerciciosPropostos/Exercicio3Aluno/Exercicio3Aluno/Aluno.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio3Aluno
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+
+        public double SomadorDeNotas()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public string ResultadoNotas()
+        {
+            var notaFinal = Nota1 + Nota2 + Nota3;
+            return notaFinal > 60.00 ? "APROVADO" : $"REPROVADO FALTAM {60 - notaFinal} PONTOS ";
+        }
+    }
+}
---
+++ ExerciciosPropostos/Exercicio3Aluno/Exercicio3Aluno/Program.cs
@@ -0,0 +1,19 @@
+﻿using Exercicio3Aluno;
+using System.Globalization;
+
+var aluno = new Aluno();
+Console.Write("Nome do Aluno: ");
+aluno.Nome = Console.ReadLine();
+
+Console.WriteLine("Digite as 3 notas do aluno: ");
+Console.Write("Digite a primeira nota: ");
+aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Digite a segunda nota: ");
+aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Digite a terceira nota: ");
+aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Nota Final: " + aluno.SomadorDeNotas());
+Console.WriteLine(aluno.ResultadoNotas());
