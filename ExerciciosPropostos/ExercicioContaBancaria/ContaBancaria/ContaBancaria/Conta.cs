b25ff2b78f79358f723557a7fa0719ba4036ef21
RichardMartins99 <77687618+RichardMartins99@users.noreply.github.com>
2023-05-26 13:22:57 -0300

    excicio ContaBancaria

---
+++ ExerciciosPropostos/ExercicioContaBancaria/ContaBancaria/ContaBancaria/Conta.cs
@@ -0,0 +1,31 @@
+﻿namespace ContaBancaria
+{
+    class Conta
+    {
+        public int NumeroConta { get; private set; }
+        public string NomeTitular { get; set; }
+        private double _saldo;
+
+        public Conta(int numeroConta, string nomeTitular, double saldo)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+            _saldo = saldo;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            _saldo -= valor + 5;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta: {NumeroConta}, Titular: {NomeTitular}, Saldo: ${_saldo}";
+        }
+    }
+}
---
+++ ExerciciosPropostos/ExercicioContaBancaria/ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,33 @@
+﻿using ContaBancaria;
+
+Console.WriteLine("Adicione o número da conta: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Adicione o nome do titular da conta: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Haverá depósito inicial (s/n)?");
+string isDepositoInicial = Console.ReadLine();
+
+double deposito = 0;
+if (isDepositoInicial == "s")
+{
+    Console.WriteLine("Entre com valor de depósito inicial: ");
+    deposito = double.Parse(Console.ReadLine());
+}
+
+var conta = new Conta(numero, nome, deposito);
+
+Console.WriteLine("Dados da conta: ");
+Console.WriteLine(conta);
+
+Console.WriteLine("Entre um valor para depósito: ");
+conta.Depositar(double.Parse(Console.ReadLine()));
+
+Console.WriteLine("Dados da conta atualizado: ");
+Console.WriteLine(conta);
+
+Console.WriteLine("Entre com um valor para saque: ");
+conta.Saque(double.Parse(Console.ReadLine()));
+Console.WriteLine("Dados da conta atualizados: ");
+Console.WriteLine(conta);
