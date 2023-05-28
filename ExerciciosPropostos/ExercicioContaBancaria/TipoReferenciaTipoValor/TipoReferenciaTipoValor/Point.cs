119f42bf4476f747d269d3d3626f8170b53841ae
RichardMartins99 <77687618+RichardMartins99@users.noreply.github.com>
2023-05-28 14:24:36 -0300

    tipo struct

---
+++ ExerciciosPropostos/ExercicioContaBancaria/TipoReferenciaTipoValor/TipoReferenciaTipoValor/Point.cs
@@ -0,0 +1,13 @@
+﻿namespace TipoReferenciaTipoValor
+{
+    struct Point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
---
+++ ExerciciosPropostos/ExercicioContaBancaria/TipoReferenciaTipoValor/TipoReferenciaTipoValor/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+Point point = new();
+point.X = 10;
+point.Y = 20;
+
+Console.WriteLine(point);
+
+point = new Point();
+Console.WriteLine(point);
